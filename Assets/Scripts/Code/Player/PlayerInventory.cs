39031503f6816ce26931903af60eccbcde13f122
Noa <npascal@gaming.tech>
2025-03-12 12:34:00 +0100

    [ADD] start inventory

--- Assets/Scripts/Code/Player/PlayerInventory.cs
+++ Assets/Scripts/Code/Player/PlayerInventory.cs
@@ -3,4 +3,11 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     public Tool EquipedTool { get; set; }
+
+    private uint x;
+    private uint y;
+
+    public GameObject slot;
+    public Transform inventoryPannel;
+
 }
